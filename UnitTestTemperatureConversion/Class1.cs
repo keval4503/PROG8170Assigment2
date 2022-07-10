e6e0d26e11c31fbdb0aa95eb2910398e0972c160
Keval Kadia <kkadia4503@conestogac.on.ca>
2022-07-09 22:42:20 -0400

    Final Code

--- UnitTestTemperatureConversion/Class1.cs
+++ UnitTestTemperatureConversion/Class1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Nunit.Framework;
+using NUnit.Framework;
 using temperatureConversion;
 
 namespace UnitTestTemperatureConversion
@@ -11,156 +11,156 @@ namespace UnitTestTemperatureConversion
     public class Class1
     {
         [Test]
-        public void ConvertCelciusToFahrenheit30()
+        public void ConvertCelciusToFahrenheit40()
         {
-            int tempdata = 30;
-            double exp = 86.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 40;
+            double expectedResult = 104.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertCelciusToFahrenheit();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertCelciusToFahrenheit80()
+        public void ConvertCelciusToFahrenheit90()
         {
-            int tempdata = 80;
-            double exp = 176.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 90;
+            double expectedResult = 194.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertCelciusToFahrenheit();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertCelciusToFahrenheit125()
+        public void ConvertCelciusToFahrenheit136()
         {
-            int tempdata = 125;
-            double exp = 257.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 136;
+            double expectedResult = 277.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertCelciusToFahrenheit();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
         //end of celcius to fahrenheit
 
         [Test]
         public void ConvertCelciusToKelvin50()
         {
-            int tempdata = 550;
-            double exp = 823.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 50;
+            double expectedResult = 323.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertCelciusToKelvin();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertCelciusToKelvin100()
+        public void ConvertCelciusToKelvin450()
         {
-            int tempdata = 860;
-            double exp = 1133;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 450;
+            double expectedResult =723.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertCelciusToKelvin();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
         public void ConvertCelciusToKelvin550()
         {
-            int tempdata = 1520;
-            double exp = 1793.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 1520;
+            double expectedResult = 1793.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertCelciusToKelvin();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
         //end of celcius to kelvin
         [Test]
-        public void ConvertKelvinToFahrenheit550()
+        public void ConvertKelvinToFahrenheit1560()
         {
-            int tempdata = 550;
-            double exp = 823.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
-            double result = _tempConversion.ConvertCelciusToKelvin();
-            Assert.AreEqual(exp, result);
+            int testData = 1560;
+            double expectedResult = 2348.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
+            double result = _tempConversion.ConvertKelvinToFahrenheit();
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertKelvinToFahrenheit1520()
+        public void ConvertKelvinToFahrenheit1650()
         {
-            int tempdata = 1520;
-            double exp = 1793.0;
-            temperatureConversion.Conversion _conv = new temperatureConversion.Conversion(tempdata);
-            double result = _tempConversion.ConvertCelciusToKelvin();
-            Assert.AreEqual(exp, result);
+            int testData = 1650;
+            double expectedResult = 2510.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
+            double result = _tempConversion.ConvertKelvinToFahrenheit();
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertKelvinToFahrenheit638()
+        public void ConvertKelvinToFahrenheit738()
         {
-            int tempdata = 638;
-            double exp = 911.0;
-            temperatureConversion.Conversion _tempConversion = new temperatureConversion.Conversion(tempdata);
-            double result = _tempConversion.ConvertCelciusToKelvin();
-            Assert.AreEqual(exp, result);
+            int testData = 738;
+            double expectedResult = 869.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
+            double result = _tempConversion.ConvertKelvinToFahrenheit();
+            Assert.AreEqual(expectedResult, result);
         }
         //end of kelvin to fahrenheit
 
         [Test]
-        public void ConvertKelvinToCelsius367()
+        public void ConvertKelvinToCelsius486()
         {
-            int tempdata = 367;
-            double exp = 94.0;
-            temperatureConversion.Conversion _tempConversion = new temperatureConversion.Conversion(tempdata);
+            int testData = 486;
+            double expectedResult = 213.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertKelvinToCelsius();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertKelvinToCelsius696()
+        public void ConvertKelvinToCelsius712()
         {
-            int tempdata = 696;
-            double exp = 423.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 712;
+            double expectedResult = 439.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertKelvinToCelsius();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertKelvinToCelsius857()
+        public void ConvertKelvinToCelsius867()
         {
-            int tempdata = 857;
-            double exp = 584.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 867;
+            double expectedResult = 594.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertKelvinToCelsius();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
         //end of kelvin to celsius
 
         [Test]
-        public void ConvertFahrenheitToKelvin59()
+        public void ConvertFahrenheitToKelvin23()
         {
-            int tempdata = 59;
-            double exp = 288.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 23;
+            double expectedResult = 268.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertFahrenheitToKelvin();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertFahrenheitToKelvin196()
+        public void ConvertFahrenheitToKelvin156()
         {
-            int tempdata = 196;
-            double exp = 363.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 156;
+            double expectedResult = 341.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertFahrenheitToKelvin();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertFahrenheitToKelvin598()
+        public void ConvertFahrenheitToKelvin789()
         {
-            int tempdata = 598;
-            double exp = 584.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 789;
+            double expectedResult = 690.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertFahrenheitToKelvin();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         //end of Fahrenheit to kelvin
@@ -168,31 +168,31 @@ namespace UnitTestTemperatureConversion
         [Test]
         public void ConvertFahrenheitToCelsius60()
         {
-            int tempdata = 60;
-            double exp = 15.00;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 60;
+            double expectedResult = 15.00;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertFahrenheitToCelsius();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertFahrenheitToCelsius85()
+        public void ConvertFahrenheitToCelsius185()
         {
-            int tempdata = 85;
-            double exp = 29.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 185;
+            double expectedResult = 84.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertFahrenheitToCelsius();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [Test]
-        public void ConvertFahrenheitToCelsius96()
+        public void ConvertFahrenheitToCelsius250()
         {
-            int tempdata = 96;
-            double exp = 35.0;
-            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(tempdata);
+            int testData = 250;
+            double expectedResult = 120.0;
+            temperatureConversion.temperatureConversion _tempConversion = new temperatureConversion.temperatureConversion(testData);
             double result = _tempConversion.ConvertFahrenheitToCelsius();
-            Assert.AreEqual(exp, result);
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
